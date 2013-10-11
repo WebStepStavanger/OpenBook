4d3fba07e8b1880d8000ff784c7a8a862c5da29f
crimp <randomcommits@gmail.com>
2013-10-11 21:45:57 +0200

    Now using cross device html5 eBook reader

--- source/server/server.js
+++ source/server/server.js
@@ -1,18 +1,20 @@
 var express = require('express'),
     routes = require('./routes'),
-    engines = require('consolidate');
+    engines = require('consolidate'),
+    partials = require('express-partials');
 
 exports.startServer = function(config, callback) {
 
   var port = process.env.PORT || config.server.port;
 
-
   var app = express();
+
   var server = app.listen(port, function() {
     console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
   });
 
   app.configure(function() {
+    app.use(partials());
     app.set('port', port);
     app.set('views', config.server.views.path);
     app.engine(config.server.views.extension, engines[config.server.views.compileWith]);
@@ -23,6 +25,7 @@ exports.startServer = function(config, callback) {
     app.use(express.compress());
     app.use(config.server.base, app.router);
     app.use(express.static(config.watch.compiledDir));
+    app.use(express.static( __dirname + '/static' ));
   });
 
   app.configure('development', function() {
@@ -30,6 +33,10 @@ exports.startServer = function(config, callback) {
   });
 
   app.get('/', routes.index(config));
+  //app.get('/read', routes.reader(config));
+  app.get('/read', function(req,res,next){
+    res.render('read.ejs',{layout:false})    
+  });
 
   callback(server);
 };
